da27f0555e5b3b24717dbcd9f161075d8f891eef
Jaedyn <jaedyn11@gmail.com>
2021-09-25 12:09:48 -0400

    Dark mode toggle (#10)

--- src/components/AboutCard.js
+++ src/components/AboutCard.js
@@ -12,53 +12,62 @@ import {
 } from '@fortawesome/free-brands-svg-icons'
 import treeImg from '../img/tree.jpg'
 
-const AboutCard = () =>
-    <Card className='mb-3'>
+const AboutCard = ({ darkMode }) => {
+    const textColor = darkMode ? 'text-light' : 'text-dark'
 
-        <Card.Img variant='top' src={treeImg} />
+    return (
+        <Card className="mb-3" bg={darkMode ? 'dark' : 'white'} border={darkMode ? 'secondary' : ''}>
 
-        <Card.ImgOverlay className='text-right text-light'>
-            <h2>Jaedyn Lee</h2>
-            <p className='font-weight-500 smaller w-md-75 w-50 float-right'>
-                full-stack developer, font enthusiast, tree hugger
-            </p>
-        </Card.ImgOverlay>
+            <Card.Img variant='top' src={treeImg} />
 
-        <Card.Body>
+            <Card.ImgOverlay className='text-right text-light'>
+                <h2>Jaedyn Lee</h2>
+                <p className='font-weight-500 smaller w-md-75 w-50 float-right'>
+                    full-stack developer, font enthusiast, tree hugger
+                </p>
+            </Card.ImgOverlay>
 
-            <div className='mb-1 text-dark'>
-                <FontAwesomeIcon icon={ faMapMarkerAlt } />
-                <span className='pl-2'>Boston, MA, USA</span>
-            </div>
+            <Card.Body>
 
-            <div className='mb-1 text-dark'>
-                <FontAwesomeIcon icon={ faEnvelopeSquare } />
-                <span className='pl-2'>jaedyn.e.lee@gmail.com</span>
-            </div>
+                <div className={`mb-1 ${textColor}`}>
+                    <FontAwesomeIcon icon={ faMapMarkerAlt } />
+                    <span className='pl-2'>Boston, MA, USA</span>
+                </div>
 
-            <div className='mb-1'>
-                <Card.Link className='text-dark' href='https://github.com/jaedynlee' target='blank'>
-                    <FontAwesomeIcon icon={ faGithubSquare } />
-                    <span className='pl-2'>github.com/jaedynlee</span>
-                </Card.Link>
-            </div>
+                <div className={`mb-1 ${textColor}`}>
+                    <FontAwesomeIcon icon={ faEnvelopeSquare } />
+                    <span className='pl-2'>jaedyn.e.lee@gmail.com</span>
+                </div>
 
-            <div className='mb-1'>
-                <Card.Link className='text-dark' href='https://linkedin.com/in/jaedynlee' target='blank'>
-                    <FontAwesomeIcon icon={ faLinkedin } />
-                    <span className='pl-2'>linkedin.com/in/jaedynlee</span>
-                </Card.Link>
-            </div>
+                <div className='mb-1'>
+                    <Card.Link className={textColor} href='https://github.com/jaedynlee' target='blank'>
+                        <FontAwesomeIcon icon={ faGithubSquare } />
+                        <span className='pl-2'>github.com/jaedynlee</span>
+                    </Card.Link>
+                </div>
 
-            <div className='text-center mt-4'>
-                <Card.Link href='../Jaedyn Lee Resume.pdf' className='btn btn-primary btn-sm round' target='blank'>
-                    <FontAwesomeIcon icon={ faFileDownload } />
-                    <span className='pl-2'>Download resume</span>
-                </Card.Link>
-            </div>
-            
-        </Card.Body>
+                <div className='mb-1'>
+                    <Card.Link className={textColor} href='https://linkedin.com/in/jaedynlee' target='blank'>
+                        <FontAwesomeIcon icon={ faLinkedin } />
+                        <span className='pl-2'>linkedin.com/in/jaedynlee</span>
+                    </Card.Link>
+                </div>
 
-    </Card>
+                <div className='text-center mt-4'>
+                    <Card.Link 
+                        href='../Jaedyn Lee Resume.pdf' 
+                        className={`btn btn-primary btn-sm round ${darkMode ? 'btn-light text-dark' : 'btn-dark'}`}
+                        target='blank'
+                    >
+                        <FontAwesomeIcon icon={ faFileDownload } />
+                        <span className='pl-2'>Download resume</span>
+                    </Card.Link>
+                </div>
+                
+            </Card.Body>
+
+        </Card>
+    )
+}
 
 export default AboutCard
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import { Row, Col, Container } from 'react-bootstrap'
+import { Button, Col, Container, Row } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faCode,
   faGraduationCap,
   faLaptopCode,
+  faMoon,
+  faSun,
   faUserCog,
   faUsers
 } from '@fortawesome/free-solid-svg-icons'
@@ -17,77 +19,98 @@ import skills from '../content/skills'
 import projects from '../content/projects'
 import AboutCard from '../components/AboutCard'
 
-const Home = () =>
-  <Container className='px-md-0 py-5'>
-    <Row>
-      <Col md='4' className='px-md-0'>
-        <div id='about' className='sticky-top'>
-          <AboutCard />
-        </div>
-      </Col>
+class Home extends React.Component {
+	constructor(props) {
+		super(props)
 
-      <Col md='8' className='pl-md-0 pl-md-5'>
+		this.state = {
+			darkMode: false,
+		}
+	}
 
-        <h4 className='section-title'>
-          <FontAwesomeIcon icon={faLaptopCode}/>
-          <span className='pl-3'>experience</span>
-        </h4>
-        {renderMilestones(milestones)}
+  onDarkModeToggle = () => {
+    this.setState({darkMode: !this.state.darkMode});
+    document.body.classList.toggle('bg-dark')
+  };
 
-        <h4 className='section-title'>
-          <FontAwesomeIcon icon={faGraduationCap}/>
-          <span className='pl-3'>education</span>
-        </h4>
-        <Milestone milestone={education[0]} id={0} />
-        <div className='small'>
-          <div>
-            <span className='font-weight-bold'>Honors: </span>
-              Summa Cum Laude, Honors College, Clinton Global Initiative University Invitee
+  render = () => (
+    <Container className={`px-md-0 py-5 ${this.state.darkMode ? 'text-light' : 'text-dark'}`}>
+      <div id='dark-mode-toggle' className='text-right'>
+        <Button variant="link" onClick={this.onDarkModeToggle} className='mode-toggle' title={this.state.darkMode ? 'Light mode' : 'Dark mode'}>
+          <FontAwesomeIcon id='sun' icon={faSun} className={this.state.darkMode ? 'inactive-icon' : 'active-icon'}/>
+          <FontAwesomeIcon icon={faMoon} className={this.state.darkMode ? 'active-icon' : 'inactive-icon'} />
+        </Button>
+      </div>
+      <Row>
+        <Col md='4' className='px-md-0'>
+          <div id='about' className='sticky-top'>
+            <AboutCard darkMode={this.state.darkMode}/>
           </div>
-          <div>
-            <span className='font-weight-bold'>Coursework: </span>
-            Web Development, Software Development, Object-Oriented Design, 
-            Algorithms, Networks and Distributed Systems, Computer Systems,
-            Programming Languages, <span> </span>
-            <a target='blank' href='https://docs.google.com/document/d/1vXNKrYmwo23BwM5wD77obiHc4_FFxH5WtO88Qt_BNfY/edit?usp=sharing'>
-              Software Project Management
-            </a><span>, </span> <a target='blank' href='https://sites.google.com/view/is3500-group-e'>
-              Information System Design and Development
-            </a>
+        </Col>
+
+        <Col md='8' className='pl-md-0 pl-md-5'>
+          <h4 className='section-title pt-0'>
+            <FontAwesomeIcon icon={faLaptopCode}/>
+            <span className='pl-3'>experience</span>
+          </h4>
+          {renderMilestones(milestones)}
+
+          <h4 className='section-title'>
+            <FontAwesomeIcon icon={faGraduationCap}/>
+            <span className='pl-3'>education</span>
+          </h4>
+          <Milestone milestone={education[0]} id={0} />
+          <div className='small'>
+            <div>
+              <span className='font-weight-bold'>Honors: </span>
+                Summa Cum Laude, Honors College, Clinton Global Initiative University Invitee
+            </div>
+            <div>
+              <span className='font-weight-bold'>Coursework: </span>
+              Web Development, Software Development, Object-Oriented Design, 
+              Algorithms, Networks and Distributed Systems, Computer Systems,
+              Programming Languages, <span> </span>
+              <a target='blank' href='https://docs.google.com/document/d/1vXNKrYmwo23BwM5wD77obiHc4_FFxH5WtO88Qt_BNfY/edit?usp=sharing'>
+                Software Project Management
+              </a><span>, </span> <a target='blank' href='https://sites.google.com/view/is3500-group-e'>
+                Information System Design and Development
+              </a>
+            </div>
           </div>
-        </div>
-        <Milestone milestone={education[1]} id={1} />
-        <div className='small'>
-          <span className='font-weight-bold'>Activities: </span>
-          Tae Kwon Do (Black Belt), Varsity Volleyball (MVP), A Cappella
-        </div>
-
-        <h4 className='section-title'>
-          <FontAwesomeIcon icon={faUsers}/>
-          <span className='pl-3'>leadership</span>
-        </h4>
-        {renderMilestones(leadership)}
-
-        <h4 className='section-title'>
-          <FontAwesomeIcon icon={faUserCog}/>
-          <span className='pl-3'>skills</span>
-        </h4>
-        {skills.map(s => 
-          <div key={s.title} className='pb-3'>
-            <span className='font-weight-bold'>{s.title}: </span>
-            {s.skills.join(', ')}
+          <Milestone milestone={education[1]} id={1} />
+          <div className='small'>
+            <span className='font-weight-bold'>Activities: </span>
+            Tae Kwon Do (Black Belt), Varsity Volleyball (MVP), A Cappella
           </div>
-        )}
 
-        <h4 className='section-title'>
-          <FontAwesomeIcon icon={faCode}/>
-          <span className='pl-3'>projects</span>
-        </h4>
-        {renderProjects(projects)}
+          <h4 className='section-title'>
+            <FontAwesomeIcon icon={faUsers}/>
+            <span className='pl-3'>leadership</span>
+          </h4>
+          {renderMilestones(leadership)}
 
-      </Col>
-    </Row>
-  </Container>
+          <h4 className='section-title'>
+            <FontAwesomeIcon icon={faUserCog}/>
+            <span className='pl-3'>skills</span>
+          </h4>
+          {skills.map(s => 
+            <div key={s.title} className='pb-3'>
+              <span className='font-weight-bold'>{s.title}: </span>
+              {s.skills.join(', ')}
+            </div>
+          )}
+
+          <h4 className='section-title'>
+            <FontAwesomeIcon icon={faCode}/>
+            <span className='pl-3'>projects</span>
+          </h4>
+          {renderProjects(projects)}
+
+        </Col>
+      </Row>
+    </Container>
+  )
+}
   
 const renderMilestones = milestones => {
   let i = 0
