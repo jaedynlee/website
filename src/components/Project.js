f42a0becaa1394e74efe58a8d5e90c522369ddfe
Jaedyn Lee <jaedyn11@gmail.com>
2024-02-19 17:01:23 -0500

    Add arrow indicators to projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,3 +1,5 @@
+import { faChevronDown, faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import { Button, Collapse } from 'react-bootstrap'
 
@@ -12,7 +14,7 @@ class Project extends React.Component {
 
     render = () => {
         const { open } = this.state
-    
+
         return (
             <div key={this.props.id} className='pb-3'>
                 <Button
@@ -23,6 +25,12 @@ class Project extends React.Component {
                     aria-controls={this.props.id}
                     aria-expanded={open}
                 >
+                    <FontAwesomeIcon
+                        className='pr-2'
+                        icon={open ? faChevronDown : faChevronRight}
+                        size="sm"
+                        width={10}
+                    />
                     <span className='font-weight-bold'>
                         {this.props.project.title}
                     </span> - {this.props.project.description}
@@ -30,10 +38,10 @@ class Project extends React.Component {
                 <Collapse in={this.state.open}>
                     <div id={this.props.id}>
                         <ul>
-                            {this.props.project.details.bullets.map(d => 
+                            {this.props.project.details.bullets.map(d =>
                                 <li key={d}>{d}</li>
                             )}
-                            {this.props.project.details.link && 
+                            {this.props.project.details.link &&
                                 <li>
                                     <a href={this.props.project.details.link.url} target='blank'>
                                         {this.props.project.details.link.text}
