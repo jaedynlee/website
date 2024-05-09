c4d7cf38751fa18fff9afa26e0e05a082e8b9244
Jaedyn Lee <jaedyn11@gmail.com>
2024-05-09 14:39:57 -0400

    Update skills

--- src/content/skills.js
+++ src/content/skills.js
@@ -1,17 +1,50 @@
-const skillsContent =
-    [
-        {
-            "title": "Languages",
-            "skills": [ 'Python', 'JavaScript', 'Java', 'Ruby', 'HTML', 'CSS', 'SQL' ]
-        },
-        {
-            "title": "Technologies",
-            "skills": [ 'FastAPI', 'React', 'Redux', 'Angular', 'Spring', 'JPA', 'MySQL', 'PostgreSQL', 'Bootstrap 4', 'Material-UI', 'Rails' ]
-        },
-        {
-            "title": "Other",
-            "skills": [ 'Git', 'CI/CD', 'JIRA', 'Agile' ]
-        }
-    ]
+const skillsContent = [
+  {
+    title: "Languages",
+    skills: [
+      "Python 3",
+      "Java",
+      "Ruby",
+      "TypeScript",
+      "JavaScript",
+      "HTML",
+      "CSS",
+      "SQL",
+      "Kotlin",
+    ],
+  },
+  {
+    title: "Technologies",
+    skills: [
+      "FastAPI",
+      "GraphQL",
+      "React",
+      "Redux",
+      "Angular",
+      "Spring",
+      "JPA",
+      "MySQL",
+      "PostgreSQL",
+      "Rails",
+      "OPA",
+      "Celery",
+      "AWS",
+      "Terraform",
+      "Bootstrap 4",
+      "Material-UI",
+    ],
+  },
+  {
+    title: "Other",
+    skills: [
+      "Software Architecture",
+      "Object-Oriented Design",
+      "Git",
+      "CI/CD",
+      "JIRA",
+      "Agile",
+    ],
+  },
+];
 
 export default skillsContent;
